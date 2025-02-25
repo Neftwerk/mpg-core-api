ef2af0f16a65604ba9328f33045bfe1009e18a3f
ElectroCERP <carloscer27r@gmail.com>
2025-02-25 09:50:40 -0300

    Remove cognito service

--- src/module/iam/authentication/infrastructure/cognito/exception/code-mismatch.exception.ts
+++
@@ -1,9 +0,0 @@
-import { UnauthorizedException } from '@nestjs/common';
-
-import { IBaseErrorInfoParams } from '@common/base/application/interface/base-error.interface';
-
-export class CodeMismatchException extends UnauthorizedException {
-  constructor(params: IBaseErrorInfoParams) {
-    super(params);
-  }
-}
--- src/module/iam/authentication/infrastructure/cognito/exception/user-not-confirmed.exception.ts
+++
@@ -1,9 +0,0 @@
-import { ForbiddenException } from '@nestjs/common';
-
-import { IBaseErrorInfoParams } from '@common/base/application/interface/base-error.interface';
-
-export class UserNotConfirmedException extends ForbiddenException {
-  constructor(params: IBaseErrorInfoParams) {
-    super(params);
-  }
-}
