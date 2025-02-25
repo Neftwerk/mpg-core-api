44e03bcec08b8300e60705c2c2e91a4d8250180f
ElectroCERP <carloscer27r@gmail.com>
2025-02-25 15:42:01 -0300

    Rename AdminMySqlRepository to AdminPostgresqlRepository

--- src/module/iam/admin/admin.module.ts
+++ src/module/iam/admin/admin.module.ts
@@ -6,7 +6,7 @@ import { AdminMapper } from '@iam/admin/application/mapper/admin.mapper';
 import { ADMIN_REPOSITORY_KEY } from '@iam/admin/application/repository/admin.repository.interface';
 import { AdminService } from '@iam/admin/application/service/admin.service';
 import { adminPermissions } from '@iam/admin/domain/admin.permission';
-import { AdminMysqlRepository } from '@iam/admin/infrastructure/database/admin.mysql.repository';
+import { AdminPostgresqlRepository } from '@iam/admin/infrastructure/database/admin.postgresql.repository';
 import { AdminSchema } from '@iam/admin/infrastructure/database/admin.schema';
 import { AdminController } from '@iam/admin/interface/admin.controller';
 import { ReadAdminPolicyHandler } from '@iam/authentication/application/policy/read-admin-policy.handler';
@@ -16,7 +16,7 @@ const policyHandlersProviders = [ReadAdminPolicyHandler];
 
 const adminRepositoryProvider: Provider = {
   provide: ADMIN_REPOSITORY_KEY,
-  useClass: AdminMysqlRepository,
+  useClass: AdminPostgresqlRepository,
 };
 
 @Module({
