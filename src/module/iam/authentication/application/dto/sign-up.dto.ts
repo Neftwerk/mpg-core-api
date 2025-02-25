10e62fb350eef187e471b57a56552b0fef89441e
ElectroCERP <carloscer27r@gmail.com>
2025-02-25 18:37:58 -0300

    Add name, surname and biography props to SignUpDto

--- src/module/iam/authentication/application/dto/sign-up.dto.ts
+++ src/module/iam/authentication/application/dto/sign-up.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 import { ISignUpDto } from '@iam/authentication/application/dto/sign-up.dto.interface';
 
@@ -13,4 +13,19 @@ export class SignUpDto implements ISignUpDto {
   @IsString()
   @IsNotEmpty()
   password: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  surname: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  biography?: string;
 }
