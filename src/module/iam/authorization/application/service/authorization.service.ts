6679108e03e3490fb172a057e36788ff0d601303
ElectroCERP <carloscer27r@gmail.com>
2025-03-06 11:33:29 -0300

    Remove Admin import from AuthorizationService

--- src/module/iam/authorization/application/service/authorization.service.ts
+++ src/module/iam/authorization/application/service/authorization.service.ts
@@ -1,21 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
-import { IAdmin } from '@iam/authorization/application/interfaces/admin-user.interface';
+import { IUser } from '@iam/authorization/application/interfaces/user.interface';
 import { AppAction } from '@iam/authorization/domain/app-action.enum';
 import { CaslAbilityFactory } from '@iam/authorization/infrastructure/casl/factory/casl-ability.factory';
 import { AppSubjects } from '@iam/authorization/infrastructure/casl/type/app-subjects.type';
-
-import { User } from '@/module/iam/user/domain/user.entity';
 
 @Injectable()
 export class AuthorizationService {
   constructor(private readonly abilityFactory: CaslAbilityFactory) {}
 
-  isAllowed(
-    user: User | IAdmin,
-    action: AppAction,
-    subject: AppSubjects,
-  ): boolean {
+  isAllowed(user: IUser, action: AppAction, subject: AppSubjects): boolean {
     if (!user) {
       return false;
     }
