9fdbb01bd7e59f1ac40853eb483c1f5a7d53ce44
Joel Orzet <joelorzt@gmail.com>
2025-03-07 09:42:13 -0300

    fix: avoid collecting coverage from unused file

--- jest.config.js
+++ jest.config.js
@@ -21,6 +21,7 @@ module.exports = {
   },
   coveragePathIgnorePatterns: [
     '<rootDir>/module/iam/authentication/infrastructure/cognito/cognito.service.ts',
+    '<rootDir>/module/iam/authentication/infrastructure/decorator/current-admin.decorator.ts',
     '<rootDir>/module/iam/authorization/infrastructure/policy/handler/generic-policy.handler.ts',
     '<rootDir>/module/iam/authorization/infrastructure/policy/guard/policy.guard.ts',
     '<rootDir>/common/base/application/mapper/dto.mapper.ts',
