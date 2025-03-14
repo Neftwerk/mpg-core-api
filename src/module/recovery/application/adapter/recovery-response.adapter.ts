a1965395bcd2be4a5fac29776640e81e05ab0805
ElectroCERP <carloscer27r@gmail.com>
2025-03-14 11:31:44 -0300

    Create Recovery response adapter

---
+++ src/module/recovery/application/adapter/recovery-response.adapter.ts
@@ -0,0 +1,42 @@
+import { ResponseSerializerService } from '@module/app/application/service/response-serializer.service';
+
+import { BaseResponseAdapter } from '@common/base/application/adapter/base-response.adapter';
+
+export class RecoveryResponseAdapter extends BaseResponseAdapter {
+  constructor(serializerService: ResponseSerializerService) {
+    super(serializerService);
+  }
+  public oneEntityResponseRecovery<ResponseDto extends object>(
+    entityName: string,
+    resource: ResponseDto,
+    relationshipsKeys: string[] = [],
+  ) {
+    const serializerService = this.getSerializerService();
+    const options = serializerService.createSerializationOptions(
+      resource,
+      relationshipsKeys,
+    );
+
+    const serializedResource = serializerService.serialize(
+      entityName,
+      { id: '', ...resource },
+      options,
+    );
+    serializerService.addSelfLink(serializedResource);
+    if (!relationshipsKeys.length) return serializedResource;
+
+    const { included } = serializerService.getIncludedData(
+      resource,
+      relationshipsKeys,
+      entityName,
+    );
+
+    return {
+      ...serializedResource,
+      included,
+      data: {
+        ...serializedResource.data,
+      },
+    };
+  }
+}
