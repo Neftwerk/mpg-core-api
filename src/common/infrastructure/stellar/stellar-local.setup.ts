963a869fc67b78ed506525da19f3f34d8342ef3a
ElectroCERP <carloscer27r@gmail.com>
2025-03-14 11:39:59 -0300

    Add setAccountSigners and getSignatureFromTransaction to stellar-local-setup for testing

--- src/common/infrastructure/stellar/stellar-local.setup.ts
+++ src/common/infrastructure/stellar/stellar-local.setup.ts
@@ -50,3 +50,67 @@ export const generateXdr = async () => {
 
   return transaction.toXDR();
 };
+
+export const getSignatureFromTransaction = async (
+  address: Keypair,
+): Promise<{ transaction: string; signature: string }> => {
+  const server = new Horizon.Server('http://localhost:8000', {
+    allowHttp: true,
+  });
+
+  const account = await server.loadAccount(address.publicKey());
+
+  const transaction = new TransactionBuilder(account, {
+    fee: BASE_FEE,
+    networkPassphrase: process.env.STELLAR_LOCAL_NETWORK_PASSPHRASE,
+  })
+    .addOperation(
+      Operation.payment({
+        destination: address.publicKey(),
+        asset: Asset.native(),
+        amount: '10',
+        source: address.publicKey(),
+      }),
+    )
+    .setTimeout(30)
+    .build();
+
+  transaction.sign(address);
+
+  return {
+    transaction: transaction.toXDR(),
+    signature: transaction.signatures[0].signature().toString('base64'),
+  };
+};
+
+export const setAccountSigners = async (
+  address: Keypair,
+  signers: { publicKey: string; weight: number }[],
+) => {
+  const server = new Horizon.Server('http://localhost:8000', {
+    allowHttp: true,
+  });
+
+  const account = await server.loadAccount(address.publicKey());
+
+  const signersOperations = signers.map((signer) =>
+    Operation.setOptions({
+      signer: {
+        weight: signer.weight,
+        ed25519PublicKey: signer.publicKey,
+      },
+    }),
+  );
+  const transaction = new TransactionBuilder(account, {
+    fee: BASE_FEE,
+    networkPassphrase: process.env.STELLAR_LOCAL_NETWORK_PASSPHRASE,
+  })
+    .addOperation(signersOperations[0])
+    .addOperation(signersOperations[1])
+    .addOperation(signersOperations[2])
+    .setTimeout(30)
+    .build();
+  transaction.sign(address);
+
+  return await server.submitTransaction(transaction);
+};
