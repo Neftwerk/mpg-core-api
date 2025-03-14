e277ca25a0fefd9a32949433992149f30b6e727b
ElectroCERP <carloscer27r@gmail.com>
2025-03-14 11:26:01 -0300

    Create RecoveryPostgresRepository

---
+++ src/module/recovery/infrastructure/database/recovery.postgresql.repository.ts
@@ -0,0 +1,26 @@
+import { Signer } from '@module/recovery/domain/signer.entity';
+import { SignerSchema } from '@module/recovery/infrastructure/database/signers.schema';
+import { IRecoveryRepository } from '@module/recovery/repository/recovery.repository.interface';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+export const RECOVERY_REPOSITORY = 'RECOVERY_REPOSITORY';
+
+export class RecoveryPostgreSqlRepository implements IRecoveryRepository {
+  constructor(
+    @InjectRepository(SignerSchema)
+    private readonly repository: Repository<Signer>,
+  ) {}
+
+  async saveOne(signer: Signer): Promise<Signer> {
+    return await this.repository.save(signer);
+  }
+
+  async getByUserExternalId(externalId: string): Promise<Signer[]> {
+    const signer = await this.repository.find({
+      where: { user: { externalId } },
+    });
+
+    return signer;
+  }
+}
