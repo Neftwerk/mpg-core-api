da3f12bb7f0f883f321300eb3aa922c9ab10ac8e
ElectroCERP <carloscer27r@gmail.com>
2025-03-14 11:53:33 -0300

    Add timeout to Submission e2e test

--- src/module/submission/__test__/submission.e2e.spec.ts
+++ src/module/submission/__test__/submission.e2e.spec.ts
@@ -14,6 +14,7 @@ import { createAccessToken } from '@test/test.util';
 
 describe('Submission Module', () => {
   let app: INestApplication;
+  jest.setTimeout(80000);
 
   const user1Token = createAccessToken({
     sub: '00000000-0000-0000-0000-000000000001',
